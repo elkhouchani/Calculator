9d2c277283f7b38f5bc0dad98df13a76c61c5c61
Siham EL KHOUCHANI <sihamelkhouchani@gmail.com>
2023-01-27 13:04:40 +0100

    Scientific calculator

--- src/com/javaoo/calculators/ScientificCalculator.java
+++ src/com/javaoo/calculators/ScientificCalculator.java
@@ -1,6 +1,29 @@
 package com.javaoo.calculators;
+import java.lang.Math;
 
 public class ScientificCalculator {
 	
+	public final static double PI=3.14159;
+	private double holdValue;
 	
+	public static double exp(double param) {
+		return Math.exp(param);
+	}
+	
+	public static double log(double param1) {
+		return Math.log(param1);
+	}
+	
+	public void putValueInMemory(double newValue) {
+		holdValue=newValue;
+	}
+	
+	public double getValueFromMemory() {
+		return holdValue;
+	}
+	
+//	public static void main (String[] args) {
+//		System.out.println(exp(4));
+//		System.out.println(+log(8));
+//	}
 }
