527e5878f42055a3544e2ec09e37ff4ec0cf5574
Siham EL KHOUCHANI <sihamelkhouchani@gmail.com>
2023-01-27 12:22:48 +0100

    Initialize

---
+++ src/com/javaoo/calculators/BasicCalculator.java
@@ -0,0 +1,20 @@
+package com.javaoo.calculators;
+
+public class BasicCalculator {
+	
+	public double add(double param1,double param2) {
+	return param1+param2;
+	}
+	
+	public double substract(double param1,double param2) {
+		return param1-param2;
+	}
+	
+	public double multiply(double param1,double param2) {
+		return param1*param2;
+	}
+	
+	public double divide(double param1,double param2) {
+		return param1/param2;
+	}
+}
