23f44886c15f6c94404af7ab6d3b3b2e554c9b0d
Siham EL KHOUCHANI <sihamelkhouchani@gmail.com>
2023-01-27 13:38:25 +0100

    Trigonometric Calculator start

--- src/com/javaoo/calculators/TrigonometricCalculator.java
+++ src/com/javaoo/calculators/TrigonometricCalculator.java
@@ -1,5 +1,27 @@
 package com.javaoo.calculators;
 
 public class TrigonometricCalculator {
-
+	public double sine(double a) {
+		return 0;
+	}
+	
+	public double cosine(double a) {
+		return 0;
+	}
+	
+	public double tangent(double a) {
+		return 0;
+	}
+	
+	public double arcsine(double a) {
+		return 0;
+	}
+	
+	public double arccosine(double a) {
+		return 0;
+	}
+	
+	public double arctangent(double a) {
+		return 0;
+	}
 }
