03ed91291ffe2fb6f34d8bafcc586591ddc52221
Siham EL KHOUCHANI <sihamelkhouchani@gmail.com>
2023-01-27 12:33:13 +0100

    Basic Calculator

--- src/com/javaoo/calculators/BasicCalculator.java
+++ src/com/javaoo/calculators/BasicCalculator.java
@@ -2,19 +2,26 @@ package com.javaoo.calculators;
 
 public class BasicCalculator {
 	
-	public double add(double param1,double param2) {
-	return param1+param2;
-	}
-	
-	public double substract(double param1,double param2) {
-		return param1-param2;
-	}
-	
-	public double multiply(double param1,double param2) {
-		return param1*param2;
-	}
-	
-	public double divide(double param1,double param2) {
-		return param1/param2;
-	}
+	public static double add(double param1,double param2) {
+		return param1+param2;
+		}
+		
+		public static double substract(double param1,double param2) {
+			return param1-param2;
+		}
+		
+		public static double multiply(double param1,double param2) {
+			return param1*param2;
+		}
+		
+		public static double divide(double param1,double param2) {
+			return param1/param2;
+		}
+		
+		public static void main(String[] args) {
+			System.out.println(add(1,5));
+			System.out.println(substract(7.50,5));
+			System.out.println(multiply(5,5));
+			System.out.println(divide(20,5));	
+		}
 }
